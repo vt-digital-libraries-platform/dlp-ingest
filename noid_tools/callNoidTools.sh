604c6f40c83c2bd666676f7044ee62972c27223a
Lee Hunter <whunter@vt.edu>
2024-02-26 13:48:20 -0500

    start moving header arrays
    add results_files added to .gitignore
    
    move headers arrays to separate json file

---
+++ noid_tools/callNoidTools.sh
@@ -0,0 +1,12 @@
+# valid args: mint, update <noid>
+command=$1
+noid=$2
+cwd=$(pwd)
+cd ./lib_files
+
+FULL_URL="https://digital.lib.vt.edu/" \
+SHORT_URL="https://idn.lib.vt.edu/" \
+API_KEY=eSIaSK2L3y3hWpmSAzPpWaAgnghnRrEVabe5KbR4  \
+API_ENDPOINT="https://2xmdyl893j.execute-api.us-east-1.amazonaws.com/Prod/" \
+python3 NoidTools.py $command $noid
+cd $cwd
---
+++ noid_tools/lib_files/NoidTools.py
@@ -0,0 +1,70 @@
+from datetime import datetime
+import os
+import sys
+import requests
+
+
+class NoidHandler:
+    def __init__(self, api_key, api_endpoint, full_url, short_url):
+        self.api_key = api_key
+        self.api_endpoint = api_endpoint
+        self.full_url = full_url
+        self.short_url = short_url
+
+    def mint_NOID(self):
+        headers = {"x-api-key": self.api_key}
+        url = os.path.join(self.api_endpoint + "mint")
+        response = requests.get(url, headers=headers)
+        print(f"mint_NOID {response.text}")
+        if response.status_code == 200:
+            res_message = (response.json())["message"]
+            start_idx = res_message.find("New NOID: ") + len("New NOID: ")
+            end_idx = res_message.find(" is created.", start_idx)
+            return res_message[start_idx:end_idx]
+        else:
+            return None
+
+    def update_NOID(self, noid, create_date):
+        headers = {"x-api-key": self.api_key}
+        print(f"update_NOID: {noid}")
+        body = (
+            "long_url="
+            + os.path.join(self.full_url, "archive", noid)
+            + "&short_url="
+            + os.path.join(self.short_url, "ark:/53696", noid)
+            + "&noid="
+            + noid
+            + "&create_date="
+            + create_date
+        )
+        url = os.path.join(self.api_endpoint + "update")
+        response = requests.post(url, data=body, headers=headers)
+        print(f"update_NOID: {response.text}")
+
+
+if __name__ == "__main__":
+    response = ""
+    api_key = os.getenv("API_KEY")
+    api_endpoint = os.getenv("API_ENDPOINT")
+    full_url = os.getenv("FULL_URL")
+    short_url = os.getenv("SHORT_URL")
+    if len(sys.argv) < 2:
+        print(f"Usage: . ./{sys.argv[0]} mint | update <noid>")
+        sys.exit(1)
+    else:
+        noidHandler = NoidHandler(api_key, api_endpoint, full_url, short_url)
+        if sys.argv[1] == "mint":
+            response = noidHandler.mint_NOID()
+            print(f"NOID: {response}")
+        elif sys.argv[1] == "update":
+            if len(sys.argv) < 3:
+                print(f"Usage: . ./{sys.argv[0]} update <noid>")
+                sys.exit(1)
+            else:
+                noid = sys.argv[2]
+                noidHandler.update_NOID(
+                    noid, datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+                )
+        else:
+            print("Invalid command")
+            sys.exit(1)
