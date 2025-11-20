7faa48950a272a1eee07ab9093ed25b18f64119c
Lee Hunter <whunter@vt.edu>
2025-11-20 11:44:50 -0500

    ui tweaks

---
+++ src/ingest_classes/digital_objects/iiif_digital_object.py
@@ -0,0 +1,19 @@
+import boto3
+from src.ingest_classes.digital_objects.generic_digital_object import GenericDigitalObject
+
+
+class IIIFDigitalObject(GenericDigitalObject):
+    def __init__(self, env, filename, bucket, assets):
+        self.env = env
+        self.filename = filename
+        self.bucket = bucket
+        self.assets = assets
+        self.s3_client = boto3.client("s3")
+        self.s3_resource = boto3.resource("s3")
+        super().__init__(
+            env, filename, bucket, assets, self.s3_client, self.s3_resource
+        )
+
+    def import_digital_objects(self):
+        print("import_digital_objects called from IIIFTypeDigitalObjects")
+        return None
---
+++ src/ingest_classes/metadata/iiif_metadata.py
@@ -0,0 +1,6 @@
+from src.ingest_classes.metadata.generic_metadata import GenericMetadata
+
+
+class IIIFMetadata(GenericMetadata):
+    def __init__(self, env, filename, bucket, assets):
+        super().__init__(env, filename, bucket, assets)
