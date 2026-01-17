df4ae478ea546092e7f7230c243e7a2d10441f86
Lee Hunter <whunter@vt.edu>
2026-01-17 10:43:58 -0500

    one more time

--- routes/pages.py
+++ routes/pages.py
@@ -4,7 +4,7 @@ from flask import redirect, render_template, session, url_for
 def index():
     user = session.get('user')
     if user:
-        return  redirect(url_for("/ingest-form"))
+        return  redirect(url_for("ingest_form"))
     else:
         return render_template("login_page.html")
     
