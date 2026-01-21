bf25930f2bccaa18e14990f09d4f5f86084d3c2e
Lee Hunter <whunter@vt.edu>
2026-01-21 06:20:46 -0500

    get log file name from env w/ fallback

--- application.py
+++ application.py
@@ -10,7 +10,7 @@ import utils.web_utils as utils
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(
-    filename='/var/log/application.log', 
+    filename=os.environ.get('LOG_FILE') or '/var/log/application.log', 
     level=logging.INFO,
     format='%(asctime)s %(levelname)-8s %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S'
--- dev_startup.sh
+++ dev_startup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 pip install -r requirements.txt
 
+LOG_FILE="log/application.log" \
 INGEST_ENV_YAML="env_defaults.yml" \
 APP_SECRET="163q19s9uqsqdus627mbhg4m6ajnpeepheonacflg17q180volbn" \
 python -m gunicorn --bind=0.0.0.0:8000 --workers 1 application
