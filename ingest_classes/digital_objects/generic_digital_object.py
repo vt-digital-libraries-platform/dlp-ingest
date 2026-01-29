c681fee0ecae8f9334706917a98d3390d57e8c09
Lee Hunter <whunter@vt.edu>
2026-01-29 12:15:00 -0500

    overhaul dig objs

--- ingest_classes/digital_objects/generic_digital_object.py
+++ ingest_classes/digital_objects/generic_digital_object.py
@@ -1,4 +1,4 @@
-import boto3, datetime, io, json, os, pathlib
+import boto3, datetime, io, json, logging, os, pathlib
 from botocore.response import StreamingBody
 from utils.s3_tools import get_matching_s3_keys
 from ingest_classes.metadata.generic_metadata import GenericMetadata
@@ -14,6 +14,8 @@ class GenericDigitalObject:
         self.s3_client = s3_client or boto3.client("s3")
         self.s3_resource = s3_resource or boto3.resource("s3")
         self.lambda_client = boto3.client("lambda")
+        self.logger = logging.getLogger()
+
 
     def import_digital_objects(self):
         metadataHandler = GenericMetadata(
@@ -23,43 +25,39 @@ class GenericDigitalObject:
         source_bucket, dest_bucket = self.get_buckets()
         df = metadataHandler.csv_to_dataframe(io.BytesIO(metadata["Body"].read()))
 
-        num_successful = 0
-        successful_copies = []
-        num_failed = 0
-        failed_copies = []
-
         source_dir = os.path.join(
             self.env["COLLECTION_CATEGORY"], self.env["COLLECTION_IDENTIFIER"]
         )
-        print("category: " + self.env["COLLECTION_CATEGORY"])
-        print("collection identifier: " + self.env["COLLECTION_IDENTIFIER"])
-        print("source_dir: " + source_dir)
-        print()
-
-        print("----------------")
-        print("Collection assets")
-        print("----------------")
+        if "INGEST_TYPE" in self.env and self.env["INGEST_TYPE"] == "collection":
         # collection assets
+            results = self.import_collection_objects(source_bucket, source_dir, dest_bucket)
+
+        if "INGEST_TYPE" in self.env and self.env["INGEST_TYPE"] == "archive":
+        # item assets
+            results = self.import_item_objects(df, source_bucket, source_dir, dest_bucket)
+
+
+
+    def import_collection_objects(self, source_bucket, source_dir, dest_bucket):
         for asset in self.assets["collection"]:
             formatted_asset = None
-            print("--------")
-            print("asset: " + asset + " = " + str(self.assets["collection"][asset]))
-            # collection: list
-            if type(self.assets["collection"][asset]) is list:
+            
+            local_assets = self.assets["collection"][asset]
+            if type(local_assets) is not list:
+                local_assets = [local_assets]
                 for item in self.assets["collection"][asset]:
+                    # exact, case sensitive search
                     formatted_asset = item.replace("<variable>", "")
                     asset_path = os.path.join(source_dir, formatted_asset)
                     success = False
                     for key in get_matching_s3_keys(
                         source_bucket.name, source_dir, formatted_asset
                     ):
-                        print(f"Collection list: {key}")
-                        print("Exact Match")
                         success = self.format_and_copy(
                             source_bucket, source_dir, key, dest_bucket
                         )
                     if not success:
-                        print("No match found, trying to ignore filename case")
+                        # case insensitive search
                         asset_path_no_filename = asset_path.replace(
                             asset_path.split("/")[-1], ""
                         )
@@ -69,114 +67,47 @@ class GenericDigitalObject:
                         ):
                             if key.lower() == asset_path.lower():
                                 matching_key = key
-                                print(f"Collection list: {key}")
-                                print(f"Case Insentive Match")
                                 success = self.format_and_copy(
                                     source_bucket, source_dir, matching_key, dest_bucket
                                 )
                         if matching_key is None:
-                            print("No match found still? I got nothing.")
+                            self.logger.info("No match found.")
 
-                        num_successful, successful_copies, num_failed, failed_copies = (
-                            self.log_copy(
-                                key,
-                                success,
-                                num_successful,
-                                successful_copies,
-                                num_failed,
-                                failed_copies,
-                            )
-                        )
-                        # end collection: list
-            # collection: string
-            else:
-                success = False
-                formatted_asset = self.assets["collection"][asset].replace(
-                    "<variable>", ""
-                )
-                asset_path = os.path.join(source_dir, formatted_asset)
-                # match on the entire key, case sensitive
-                for key in get_matching_s3_keys(
-                    source_bucket.name, source_dir, formatted_asset
-                ):
-                    print(f"Collection string: {key}")
-                    print("Exact Match")
-                    success = self.format_and_copy(
-                        source_bucket, source_dir, key, dest_bucket
-                    )
-                if not success:
-                    print("No match found, trying to ignore filename case")
-                    asset_path_no_filename = asset_path.replace(
-                        asset_path.split("/")[-1], ""
-                    )
-                    matching_key = None
-                    for key in get_matching_s3_keys(
-                        source_bucket.name, asset_path_no_filename
-                    ):
-                        if key.lower() == asset_path.lower():
-                            matching_key = key
-                            print(f"Collection string: {key}")
-                            print(f"Case Insentive Match")
-                            success = self.format_and_copy(
-                                source_bucket, source_dir, matching_key, dest_bucket
-                            )
-                    if matching_key is None:
-                        print("No match found still? I got nothing.")
 
-                num_successful, successful_copies, num_failed, failed_copies = (
-                    self.log_copy(
-                        key,
-                        success,
-                        num_successful,
-                        successful_copies,
-                        num_failed,
-                        failed_copies,
-                    )
-                )
-        print("----------------")
-        print("Item assets")
-        print("----------------")
 
+    def import_item_objects(self, df, source_bucket, dest_bucket):
         # item assets
         for idx, row in df.iterrows():
-            print("\n========")
-            print("identifier: " + row.identifier)
             source_dir, dest_dir = self.get_bucket_paths(row)
             for asset in self.assets["item"]:
                 success = False
-                print("\n----")
-                print("asset: " + asset + " = " + str(self.assets["item"][asset]))
                 if asset == "thumbnail" and self.env["GENERATE_THUMBNAILS"]:
-                    print("Thumbnail generation is set. Skipping copy.")
                     continue
+                
                 formatted_asset = None
-                # item: list
-                if type(self.assets["item"][asset]) is list:
-                    for item in self.assets["item"][asset]:
-                        formatted_asset = item.replace(
-                            "<item_identifier>", row["identifier"]
-                        ).replace("<variable>", "")
-                        for key in get_matching_s3_keys(
-                            source_bucket.name, source_dir, formatted_asset
-                        ):
-                            print(f"Item list: {key}")
-                            print("Exact Match")
+                local_assets = self.assets["item"][asset]
+                if type(local_assets) is not list:
+                    local_assets = [local_assets]
+                    for item in local_assets:
+                        success = False
+                        formatted_asset = (
+                            self.assets["item"][asset]
+                                .replace("<item_identifier>", row["identifier"])
+                                .replace("<variable>", "")
+                        )
+
+
+                        for key in get_matching_s3_keys(source_bucket.name, source_dir, formatted_asset):
                             success = self.format_and_copy(
-                                source_bucket,
-                                source_dir,
-                                key,
-                                dest_bucket,
-                                dest_dir,
+                                source_bucket, source_dir, key, dest_bucket, dest_dir
                             )
+
                         if not success:
-                            print("No match found, trying to ignore filename case")
-                            asset_path = os.path.join(source_dir, formatted_asset)
+                            asset_path = os.path.join(source_dir, "")
                             matching_key = None
-                            for key in get_matching_s3_keys(source_bucket, source_dir):
-                                if key.lower() == asset_path.lower():
+                            for key in get_matching_s3_keys(source_bucket.name, source_dir):
+                                if key.lower() == asset_path.lower() and not key.endswith("/"):
                                     matching_key = key
-                                    print(f"Item list: {key}")
-                                    print(f"Case Insentive Match")
                                     success = self.format_and_copy(
                                         source_bucket,
                                         source_dir,
@@ -184,95 +115,29 @@ class GenericDigitalObject:
                                         dest_bucket,
                                     )
                             if matching_key is None:
-                                print("No match found still? I got nothing.")
+                                self.logger.error(f"No match found for identifier {row['identifier']}")
 
-                        (
-                            num_successful,
-                            successful_copies,
-                            num_failed,
-                            failed_copies,
-                        ) = self.log_copy(
-                            key,
-                            success,
-                            num_successful,
-                            successful_copies,
-                            num_failed,
-                            failed_copies,
-                        )
-                        # end item: list
-                        if success:
-                            print("GREAT SUCCESS")
-                # item: string
-                else:
-                    success = False
-                    formatted_asset = (
-                        self.assets["item"][asset]
-                        .replace("<item_identifier>", row["identifier"])
-                        .replace("<variable>", "")
-                    )
-                    print(f"Formatted asset: {formatted_asset}")
-
-                    for key in get_matching_s3_keys(
-                        source_bucket.name, source_dir, formatted_asset
-                    ):
-                        print("Exact Match")
-                        success = self.format_and_copy(
-                            source_bucket, source_dir, key, dest_bucket, dest_dir
-                        )
-                        print(f"Copy success: {str(success)}")
-                    if not success:
-                        print("No match found, trying to ignore filename case")
-                        asset_path = os.path.join(source_dir, "")
-                        matching_key = None
-                        for key in get_matching_s3_keys(source_bucket.name, source_dir):
-                            if key.lower() == asset_path.lower() and not key.endswith(
-                                "/"
+                        # Generate a thumbnail for the object if requested
+                        try:
+                            if (
+                                success
+                                and self.env["GENERATE_THUMBNAILS"]
+                                and key.endswith(self.assets["options"]["asset_src"])
                             ):
-                                matching_key = key
-                                print(f"Case Insentive Match")
-                                success = self.format_and_copy(
-                                    source_bucket,
-                                    source_dir,
-                                    matching_key,
-                                    dest_bucket,
+                                target_key = os.path.join(dest_dir, os.path.basename(key))
+                                thumbnail_key = target_key.replace(
+                                    f".{self.assets['options']['asset_src']}",
+                                    "_thumbnail.jpg",
                                 )
-                                print(f"Copy success: {str(success)}")
-                        if matching_key is None:
-                            print("No match found still? I got nothing.")
+                                self.call_thumbnail_service(
+                                    self.env["AWS_SRC_BUCKET"],
+                                    key,
+                                    self.env["AWS_DEST_BUCKET"],
+                                    thumbnail_key,
+                                )
+                        except Exception as e:
+                            self.logger.info(e)
 
-                    (
-                        num_successful,
-                        successful_copies,
-                        num_failed,
-                        failed_copies,
-                    ) = self.log_copy(
-                        key,
-                        success,
-                        num_successful,
-                        successful_copies,
-                        num_failed,
-                        failed_copies,
-                    )
-                    try:
-                        if (
-                            success
-                            and self.env["GENERATE_THUMBNAILS"]
-                            and key.endswith(self.assets["options"]["asset_src"])
-                        ):
-                            target_key = os.path.join(dest_dir, os.path.basename(key))
-                            thumbnail_key = target_key.replace(
-                                f".{self.assets['options']['asset_src']}",
-                                "_thumbnail.jpg",
-                            )
-                            self.call_thumbnail_service(
-                                self.env["AWS_SRC_BUCKET"],
-                                key,
-                                self.env["AWS_DEST_BUCKET"],
-                                thumbnail_key,
-                            )
-                    except Exception as e:
-                        print(e)
-        self.log_results(num_successful, successful_copies, num_failed, failed_copies)
 
     def call_thumbnail_service(self, src_bucket, src_key, dest_bucket, dest_key):
         payload = {
@@ -281,23 +146,23 @@ class GenericDigitalObject:
             "dest_bucket": dest_bucket,
             "dest_key": dest_key,
         }
-        print("Calling thumbnail service with payload:")
-        print(payload)
+        self.logger.info("Calling thumbnail service with payload:")
+        self.logger.info(payload)
         response = self.lambda_client.invoke(
             FunctionName="vtdlp-thumbnail-service",
             InvocationType='Event',
             Payload=bytes(json.dumps(payload), "utf8"),
         )
-        print("Thumbnail service response:")
-        print(response)
+        self.logger.info("Thumbnail service response:")
+        self.logger.info(response)
 
     def log_results(self, num_successful, successful_copies, num_failed, failed_copies):
         success_msg = f"Successfully copied {num_successful} files.\n"
-        print()
-        print("----------------")
-        print(success_msg)
+        self.logger.info()
+        self.logger.info("----------------")
+        self.logger.info(success_msg)
         failed_msg = f"Failed to copy {num_failed} files.\n"
-        print(failed_msg)
+        self.logger.info(failed_msg)
         now = datetime.datetime.now()
         output_path = os.path.join(self.env["SCRIPT_ROOT"], "results_files")
         pathlib.Path(output_path).mkdir(parents=True, exist_ok=True)
@@ -335,13 +200,13 @@ class GenericDigitalObject:
 
     def s3_copy(self, source_bucket, source_key, dest_bucket, dest_key):
         if self.env["VERBOSE"]:
-            print("Copying:")
-            print(f"{source_bucket.name}:{source_key}")
-            print("to:")
-            print(f"{dest_bucket.name}:{dest_key}")
+            self.logger.info("Copying:")
+            self.logger.info(f"{source_bucket.name}:{source_key}")
+            self.logger.info("to:")
+            self.logger.info(f"{dest_bucket.name}:{dest_key}")
         if not dest_key.endswith("/"):
             if self.env["DRY_RUN"]:
-                print(f"DRYRUN: s3 copy: simulated")
+                self.logger.info(f"DRYRUN: s3 copy: simulated")
                 return True
             else:
                 try:
@@ -354,7 +219,7 @@ class GenericDigitalObject:
                     )
                     return True
                 except Exception as e:
-                    print(e)
+                    self.logger.info(e)
                     return False
             
         else:
